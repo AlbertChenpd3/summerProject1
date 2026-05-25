b295d1f44a5d069457478597f996ca3ba15a9179
Albert <albchen223@gmail.com>
2026-05-25 19:54:22 -0400

    testing

--- summerProject.py
+++ summerProject.py
@@ -1,8 +1,19 @@
+#python -m flask --app summerProject run    
+import os
 import psutil
 from flask import Flask
 
-for proc in psutil.process_iter(['pid', 'name', 'username']): # does print out roblox player
-    if (proc.username != "None"):
+def find_procs_by_name(name): #maybe find with a .exe file
+    "Return a list of processes matching 'name'."
+    ls = []
+    for p in psutil.process_iter(["name", "exe", "cmdline"]):
+        if name == p.info['name'] or \
+                p.info['exe'] and os.path.basename(p.info['exe']) == name or \
+                p.info['cmdline'] and p.info['cmdline'][0] == name:
+            ls.append(p)
+    return ls
+
+for proc in psutil.process_iter(['pid', 'name']): # does print out roblox player
         print(proc.info)
 app = Flask(__name__)
 
