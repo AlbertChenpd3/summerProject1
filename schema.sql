4b69d2250d4eb4e378e0075284af48c0bf2d095b
Albert <albchen223@gmail.com>
2026-07-20 22:57:59 -0400

    added some stuff to the sql file to hash

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
 --Creates the databases used to store rooms and within those rooms the status of each computer
+DROP TABLE IF EXISTS roomList;
+DROP TABLE IF EXISTS individualRooms;
+
+CREATE TABLE roomList(
+    roomName varchar(255) PRIMARY KEY, --ensures unique names
+    roomHash INTEGER PRIMARY KEY AUTOINCREMENT --linked to each hash, should generate a unique one for each room
+
+);
+
+CREATE TABLE individualRooms(
+    roomHash INTEGER PRIMARY KEY --should link to roomList room Hash, also should be the row not the column
+    --need to store multiple values here somehow
+);
